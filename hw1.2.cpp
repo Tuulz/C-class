f939237515a6c708a2a5abd256b80e01617ad55e
Tuulz <68292073+Tuulz@users.noreply.github.com>
2021-03-04 14:33:45 -0500

    Add files via upload

---
+++ hw1.2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+	int x = 20;
+	int y = 15; 
+	int z = x / y;
+	cout<<"z = ";
+	
+	cout<<z<<endl;
+	
+	cout<<"x + z =";
+	cout<<x + z;
+
+
+	return 0;
+}
+
---
+++ hw1.3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+	int x = 20;
+	int y = 15; 
+	int z = x - y;
+	cout<<"z = ";
+	
+	cout<<z<<endl;
+	
+	cout<<"x + z =";
+	cout<<x + z;
+
+
+	return 0;
+}
